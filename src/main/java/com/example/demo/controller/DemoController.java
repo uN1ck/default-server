7b49d6d816b02990caf165e41977c714f08483bf
Siberian Barbarian <qserxsdiv@gmail.com>
2019-07-11 13:51:58 +0700

    Sonarfix

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.pow.PowProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +13,12 @@ public class DemoController {
     private final PowProvider powProvider;
 
 
-    @RequestMapping("/ping")
+    @RequestMapping(value = "/ping", method = RequestMethod.GET)
     public String ping() {
         return "pong";
     }
 
-    @RequestMapping("/pow")
+    @RequestMapping(value = "/pow", method = RequestMethod.GET)
     public String getPow(@RequestParam int value) {
         return String.valueOf(powProvider.getPow(value));
     }
